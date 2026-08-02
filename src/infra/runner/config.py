c29e67da7175cf72470cd5790c95dadf2e095010
DylanCoon99 <dylcoon99@gmail.com>
2026-08-02 10:41:59 -0400

    finsished config; yaml parsing and validation

--- src/infra/runner/config.py
+++ src/infra/runner/config.py
@@ -1,5 +1,6 @@
 from dataclasses import dataclass, field
 from errors import MalformedConfig
+import yaml
 
 
 '''
@@ -21,61 +22,51 @@ class Config:
 	name: str
 	weights: str
 	runtime: str
-	input_resolution: list[str] = field(default_factory=list)
 	quantization: str
 	preprocessing: str
+	input_resolution: list[int] = field(default_factory=list)
 
 
-    @classmethod
-    def from_path(cls, yaml_path: str):
+	@classmethod
+	def from_path(cls, yaml_path: str):
 
-    	with open(yaml_path, "r") as file:
-    		# loads the yaml to a dictionary
-		    config = yaml.safe_load(file)
+		try:
+			with open(yaml_path, "r") as file:
+				# loads the yaml to a dictionary
+				config = yaml.safe_load(file)
+
+		except FileNotFoundError:
+			raise FileNotFoundError(f"File: {yaml_path} not found!")
 
 		# validate the yaml is the correct format
+
+		return cls.from_dict(config)
+
+
+
+	@classmethod
+	def from_dict(cls, yaml_dict):
 		try:
-			name             = config["name"]
-			weights          = config["weights"]
-			runtime          = config["runtime"]
-			# I am wondering if the types are inferred when the yaml is load; this is supposed to be a list
-			# I hope it is not a string; need to check
-			input_resolution = config["input_resolution"]
-			quantization     = config["quantization"]
-			preprocessing    = config["preprocessing"]
 
-			return cls(
-				name=name, 
-				weights=weights,
-				runtime=runtime,
-				input_resolution=input_resolution,
-				quantization=quantization,
-				preprocessing=preprocessing
-			)
-		
-		except Exception as e:
-			raise MalformedConfig(error_msg=e)
+			# validate the yaml
+			# need to check if the values are none
 
-
-    @classmethod
-    def from_dict(cls, yaml_dict):
-        try:
 			name             = yaml_dict["name"]
 			weights          = yaml_dict["weights"]
 			runtime          = yaml_dict["runtime"]
-			input_resolution = yaml_dict["input_resolution"]
 			quantization     = yaml_dict["quantization"]
 			preprocessing    = yaml_dict["preprocessing"]
+			input_resolution = yaml_dict["input_resolution"]
 
 			return cls(
 				name=name, 
 				weights=weights,
 				runtime=runtime,
-				input_resolution=input_resolution,
 				quantization=quantization,
-				preprocessing=preprocessing
+				preprocessing=preprocessing,
+				input_resolution=input_resolution
 			)
 		
-		except Exception as e:
+		except KeyError as e:
 			raise MalformedConfig(error_msg=e)
 
--- src/infra/runner/main.py
+++ src/infra/runner/main.py
@@ -1,3 +1,48 @@
 # Model Evaluation Runner Implementation
+from config import Config
+from errors import MalformedConfig
 
 
+# Testing the config yaml parsing
+
+
+
+
+def main():
+
+	file_path = "/Users/Dylan/Documents/gaze_detection/src/infra/runner/test_config_in.yaml"
+
+
+	try:
+		config = Config.from_path(file_path)
+	except FileNotFoundError as e:
+		print(e)
+		return
+	except MalformedConfig as e:
+		print(e)
+		return
+
+
+	# should be able to safely access keys at this point
+	name             = config.name
+	weights          = config.weights
+	runtime          = config.runtime
+	input_resolution = config.input_resolution
+	quantization     = config.quantization
+	preprocessing    = config.preprocessing
+
+	print(f"name: {name}")
+	print(f"weights: {weights}")
+	print(f"runtime: {runtime}")
+	print(f"input_resolution: {input_resolution}")
+	print(f"quantization: {quantization}")
+	print(f"preprocessing: {preprocessing}")
+
+	return
+
+
+
+
+
+if __name__ == "__main__":
+	main()
